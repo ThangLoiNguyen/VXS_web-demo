d04c57fe5632956f4777617b7f54babe3a99f376
thangloinguyen <thangloinguyen1209@gmail.com>
2025-03-05 14:44:04 +0700

    -mUpdate 5/3

--- src/Components/MobileMenu.tsx
+++ src/Components/MobileMenu.tsx
@@ -9,6 +9,8 @@ import Image from 'next/image'
 export default function MobileMenu() {
     const [isOpenMenu, setIsOpenMenu] = React.useState(false);
     const toggleMenu = () => setIsOpenMenu(!isOpenMenu);
+    const [isOpenSearch, setIsOpenSearch] = React.useState(false);
+    const toggleSearch = () => setIsOpenSearch(!isOpenSearch);
     const pathname = usePathname();
     return (
         <div className='sticky z-40 top-0 bg-white flex lg:hidden flex-row h-24 2xl:h-32 w-full gap-10 px-3 md:px-10 items-center shadow select-none'>
@@ -18,10 +20,17 @@ export default function MobileMenu() {
                         className='w-16 2xl:w-20 rounded-2xl overflow-hidden object-contain' />
                     <span className='text-sm 2xl:text-xl font-sans font-semibold w-[125px] 2xl:w-[200px] text-black'>CÔNG TY CỔ PHẦN CÔNG NGHỆ VXS</span>
                 </Link>
-                <div className='flex flex-row items-center gap-10'>
-                    <div className='relative hidden md:flex'>
-                        <input type="text" placeholder='Tìm kiếm' className='md:w-[200px] xl:w-[300px] border border-gray-300 py-2 px-5 pr-12 rounded-full duration-600 outline-none focus:border focus:border-gray-500' />
-                        <FontAwesomeIcon icon={faSearch} className='absolute top-1/2 -translate-y-1/2 right-5 text-gray-500 cursor-pointer' />
+                <div className='flex flex-row items-center gap-2 md:gap-10'>
+                    <div className='relative flex'>
+                        <input type="text" placeholder='Tìm kiếm' className='hidden md:flex md:w-[200px] xl:w-[300px] border border-gray-300 py-2 px-5 pr-12 rounded-full duration-600 outline-none focus:border focus:border-gray-500' />
+                        <label htmlFor="search">
+                            <FontAwesomeIcon icon={faSearch} onMouseDown={toggleSearch}
+                                className='absolute p-2 md:p-0 scale-90 ring-1 ring-gray-400 text-white bg-[black] md:bg-white rounded-full md:ring-0 top-1/2 -translate-y-1/2 right-5 md:text-gray-500 cursor-pointer' />
+                        </label>
+                        {isOpenSearch &&
+                            <input id='search' type="text" placeholder='Tìm kiếm' onBlur={() => setIsOpenSearch(false)}
+                                className='absolute md:hidden flex top-14 -right-5 outline-none border-2 border-white ring-1 shadow-2xl ring-gray-300 focus:ring-black px-3 w-[90vw] py-2 rounded animate-fade-down placeholder:text-sm text-sm' />
+                        }
                     </div>
                     <FontAwesomeIcon icon={faBars} onClick={toggleMenu}
                         className='text-2xl text-black' />
