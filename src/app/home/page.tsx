94c83c6f279b4508f0f98c8612f948f62aab3286
thangloinguyen <thangloinguyen1209@gmail.com>
2025-03-24 11:26:50 +0700

    -mbutton update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   return (
     <div className='relative'>
       <ContactSocial />
-      <MobileMenu/>
+      <MobileMenu />
       <Navigation />
       <div className='relative mb-80'>
         <Image src={slideIntro} alt="slide-intro" className='animate-fade-left-right w-11/12' />
@@ -23,17 +23,25 @@ export default function HomePage() {
             <p className='w-3/4 text-xs md:text-base 2xl:text-xl font-light font-sans tracking-wider text-black'>I&apos;m a paragraph. Click here to add your own text and edit me. click “Edit Text” or double click me to add your own content and make changes to the font. I&apos;m a great place for you to tell a story and let your users know a little more about you.</p>
           </div>
           <span className='flex justify-end'>
-            <Link href={'/about'} className=' text-white text-sm 2xl:text-lg hover:bg-[--secondary-color] border px-3 py-1 bg-[--primary-color] duration-300'>See more</Link>
+            <Link href={'/about'} className="flex items-center justify-center bg-sky-600 xl:bg-white w-28 xl:w-48 rounded-2xl h-10 xl:h-12 relative text-white xl:text-black text-sm xl:text-base font-sans font-semibold border group" type="button">
+              <p className=" xl:-translate-x-2 -translate-y-[1px]">See more</p>
+              <div className="bg-sky-600 rounded-xl h-10 xl:h-12 w-1/4 hidden xl:flex items-center justify-center absolute right-0 top-0 group-hover:w-full z-10 duration-500 rotate-180">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="25px" width="25px">
+                  <path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="white" />
+                  <path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" fill="white" />
+                </svg>
+              </div>
+            </Link>
           </span>
         </div>
       </div>
-      <AboutItems/>
+      <AboutItems />
       <div className='hidden md:flex w-full justify-center px-5 pt-16 pb-5'>
         <div className='w-full px-10 py-5 border-b border-gray-300'>
           <span className='text-3xl font-semibold py-[18px] border-b border-sky-500'>Sản phẩm tiêu biểu</span>
         </div>
       </div>
-      <SlideProduct/>
+      <SlideProduct />
       <Footer />
     </div>
   )
